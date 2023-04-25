70dd0b0903839f74f9409695f316935e88dc1799
Matheus Franco <cajondefox@gmail.com>
2023-04-25 00:01:01 -0300

    refact(components): changed header component to use appbar from paper style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,37 +1,17 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
-import { IconButton, Text, useTheme } from 'react-native-paper';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
-
-import styles from './styles';
-import { ICON_SIZE } from '../constants';
+import { Appbar } from 'react-native-paper';
 
 export default function Header({ title, back = false, config = false }) {
-    const { colors } = useTheme();
     const navigation = useNavigation<NavigationProp<any>>();
-    const style = styles(colors);
-
     const goToBack = () => navigation.goBack();
     const goToConfig = () => navigation.navigate('Configuration');
 
-    return <View style={style.container}>
-        <StatusBar />
-        <View style={style.container}>
-            { back && <IconButton
-                    testID='back-button'
-                    iconColor={colors.onPrimaryContainer}
-                    icon="chevron-left"
-                    size={ICON_SIZE}
-                    onPress={goToBack}
-            />}
-            <Text style={style.title} testID='title'>{title}</Text>
-        </View>
-        {config && <IconButton
-                testID='config-button'
-                iconColor={colors.onPrimaryContainer}
-                icon="cog"
-                size={ICON_SIZE}
-                onPress={goToConfig}
-        />}
-    </View>
+    return (
+        <Appbar.Header>
+            { back && <Appbar.BackAction onPress={() => {goToBack}} />}
+            <Appbar.Content title={title} />
+            {config && <Appbar.Action icon="cog" onPress={goToConfig} />}
+        </Appbar.Header>
+    );
 }
